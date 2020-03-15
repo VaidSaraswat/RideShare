451aec49f0dfcf331a58405634d600845ba8f884
Vaid Saraswat <vaidsaraswat@gmail.com>
2020-03-15 00:55:21 -0400

    updated ride controller

--- Server/controllers/rideController.js
+++ Server/controllers/rideController.js
@@ -14,4 +14,30 @@ router.route('/api/rides')
     })
   })
 
+  .put((req, res)=>{
+    let ride = new Ride();
+    ride.departingDate = req.body.departingDate;
+    ride.departingLocation = req.body.departingLocation;
+    ride.departingTime = req.body.departingTime;
+    ride.arrivingLocation = req.body.arrivingLocation;
+    ride.price =  req.body.price;
+    if(req.body.dropOffAlong.toLowerCase() === 'yes'){
+      ride.dropOffAlong = true;
+    } 
+    else{
+      ride.dropOffAlong = false;
+    }
+    let driverInfo = {name: req.body.name, number: req.body.number};
+    ride.driverInfo = driverInfo;
+
+    ride.save((err)=>{
+      if(err){
+        res.send(err);
+      }
+      else{
+        res.json({message: 'Ride was created successfully'});
+      }
+    })
+  })
+
 module.exports = router;
