fac896e61e6b2e5c6024fcca2218e643eded6b21
Vaid Saraswat <vaidsaraswat@gmail.com>
2020-03-21 14:11:49 -0400

    fixed user controller

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user.js');
 
-router.route('/api/users')
+async function isUniqueName(name){
+  let exists = await User.exists({name: name});
+  if(exists){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
+router.route('/api/users')
   //Get all the users
   .get((req, res)=>{
     User.find((err, users)=>{
@@ -20,8 +29,8 @@ router.route('/api/users')
   //Create user
   .put(async (req, res)=>{
     //Check if the username is currently available if so then add to the db, otherwise notify them to use a different username
-    let exists = await User.exists({name: req.body.name});
 
+    let exists = await isUniqueName(req.body.name);
     if(exists){
       res.send('Sorry that user name is already taken!');
     }
@@ -53,39 +62,6 @@ router.route('/api/users')
     }
   })
 
-  //Update user information
-  .post(async (req, res)=>{
-    let newUserInfo = {};
-    if(req.body.newName == ''){
-      newUserInfo.name = req.body.oldName;
-    }
-    else{
-      newUserInfo.name = req.body.newName;
-    }
-    if(req.body.newNumber == ''){
-      newUserInfo.number = req.body.oldNumber;
-    }
-    else{
-      newUserInfo.number = req.body.newNumber;
-    }
-    if(req.body.newPassword == ''){
-      newUserInfo.password = req.body.oldPassword;
-    }
-    else{
-      const hashedPassword = await bcrypt.hash(req.body.newPassword, 10);
-      newUserInfo.password = hashedPassword;
-    }
-
-    User.findOneAndUpdate({name: req.body.oldName}, newUserInfo, (err)=>{
-      if(err){
-        res.send(err);
-      }
-      else{
-        res.send('Your information is updated');
-      }
-    });
-  })
-
   //Delete user from system
   .delete((req, res)=>{
     User.findOneAndRemove({name: req.body.name}, (err)=>{
@@ -98,26 +74,89 @@ router.route('/api/users')
     });
   });
 
-  router.route('/api/users/login')
-    .post(async (req, res)=>{
-     let user = await User.findOne({name: req.body.name});
-      
-      if(user == null){
-        res.send('Cannot find user!');
+router.route('/api/users/login')
+  .post(async (req, res)=>{
+    let user = await User.findOne({name: req.body.name});
+    
+    if(user == null){
+      res.send('Cannot find user!');
+    }
+    else{
+      try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+          res.send('Success!');
+        }
+        else{
+          res.send('Not Allowed!');
+        } 
+      }
+      catch{
+        res.send('Error!');
+      }
+    }
+  });
+  
+router.route('/api/users/updateName')
+  .post(async (req, res)=>{
+    //Check if the name is unique, if it is then update the name otherwise send error indicating that the name already exists
+    let exists = await isUniqueName(req.body.newName);
+
+    if(exists){
+      res.send('Sorry that name already exists');
+    }
+
+    else{
+      User.findOneAndUpdate({name: req.body.oldName}, {name: req.body.newName}, (err)=>{
+        if(err){
+          res.send(err);
+        }
+        else{
+          res.send('Your username was updated successfully!');
+        }
+      });
+    }
+  });
+
+router.route('/api/users/updateNumber')
+  .post((req, res)=>{
+    User.findOneAndUpdate({name: req.body.name}, {number: req.body.newNumber}, (err)=>{
+      if(err){
+        res.send(err);
       }
       else{
-        try{
-          if(await bcrypt.compare(req.body.password, user.password)){
-            res.send('Success!');
-          }
-          else{
-            res.send('Not Allowed!');
-          } 
-        }
-        catch{
-          res.send('Error!');
-        }
+        res.send('Your phone number was updated successfully');
       }
     });
+  });
+
+router.route('/api/users/updatePassword')
+  .post(async (req, res)=>{
+    let user = await User.findOne({name: req.body.name});
+    if(user == null){
+      res.send('Invalid Username!');
+    }
+    else{
+      try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+
+          const hashedPassword = await bcrypt.hash(req.body.newPassword, 10);
+          User.findOneAndUpdate({name: req.body.name}, {password: hashedPassword}, (err)=>{
+            if(err){
+              res.send(err);
+            }
+            else{
+              res.send('Your password was updated successfully!');
+            }
+          });
+        }
+        else{
+          res.send('Invalid Credentials!');
+        } 
+      }
+      catch{
+        res.send('Error!');
+      }
+    }
+  });
 
   module.exports = router;
